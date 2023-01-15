550cc74ec5117625cff34d560e5cc9061b90851d
Khushal747 <khushal7477@gmail.com>
2023-01-15 22:18:45 +0530

    some cahnges in diff.cpp

--- LearnGit/diff.cpp
+++ LearnGit/diff.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 using namespace std;
 
-class hello {
-cout<<"hello class here";
+class A {
+    public:
+    void hello(){
+cout<<"hello class here"<<endl;
+cout<<"hello class here"<<endl;
+
+    }
 
 };
